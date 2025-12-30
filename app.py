67c30fbcbb68b6a2a642df2a2c646eaf2ff85d94
faizruzain <faizruzain@hotmail.com>
2025-12-30 21:43:32 +0700

    mocking http requests

--- app.py
+++ app.py
@@ -1,30 +1,20 @@
-import sys
-sys.path.append('my_modules')
 import asyncio
-import myModule
-from myModule import read_csv, coba
+import sys
+import requests
+import pandas as pd
+import json
+sys.path.append('my_modules')
+from myModule import read_csv, get_CPU_usage, get_temperature_records, get_memory_usage
 
 async def main():
-    await read_csv()
-    await coba()
+    df = await read_csv()
+    await get_CPU_usage(df)
+    # await get_temperature_records(df)
+    # await get_memory_usage(df)
 
 asyncio.run(main())
 
-
-
-
-
-
-
-
-
-
-
-
-# async def test():
-#     await satu()
-#     await dua() 
-
-# asyncio.run(test())
-
-
+# r = requests.get(f'https://jsonplaceholder.typicode.com/users/').json()
+# # print(r)
+# with open('cuki.json', 'w') as f:
+#     json.dump(r, f)
--- my_modules/myModule.py
+++ my_modules/myModule.py
@@ -1,6 +1,8 @@
 import os
 import pandas as pd
 import requests
+from tabulate import tabulate
+
 
 async def read_csv():
     try:
@@ -8,38 +10,49 @@ async def read_csv():
         rel_path = "../csv/device_id.csv"
         abs_file_path = os.path.join(script_dir, rel_path)
         df = pd.read_csv(abs_file_path)
+        return df
+    except NameError:
+        print(NameError)
+
+async def get_CPU_usage(df):
+    try:
+        CPU_usage = df.get(["hostname", "cpu_id"])
+        for _ in CPU_usage.itertuples(index=False):
+            # make API req
+            r = requests.get(f'https://jsonplaceholder.typicode.com/users/9')
+            if r.status_code == 200:
+                print(r)
+
+                # print results
+                print(tabulate([
+                    [_[0], _[1], 'Done']
+                ],
+                    headers=['hostname', 'cpu_id', 'status'],
+                    tablefmt="github"
+                    )
+                )
+
+                print('\n')
+            else:
+                print(f'Error occurred with status code: {r.status_code}')
+                break
+    except NameError:
+        print(NameError)
+        
+
+async def get_temperature_records(df):
+    try:
         print(df)
     except NameError:
         print(NameError)
     else:
-        print("Reading csv file: Done")
-    # print(df)
-    # print("\n")
-    # print(df.get(["hostname", "ping_id"]))
-    # for _ in df.itertuples(index=False):
-    #     print(_[0]) #kalau mau mulai dari index 0, tambahkan argumen index=False pada .itertuples()
+        print("Getting Temperature Records: Done")
 
-async def coba():
+
+async def get_memory_usage(df):
     try:
-        # r = requests.get("https://dummyjson.com/PRTG?delay=1000")
-        r = requests.get("https://api.github.com")
-        print(r.json())
+        print("get_memory_usage")
     except NameError:
         print(NameError)
     else:
-        print("HTTP Get Request: Done")
-   
-
-
-
-
-
-# async def satu():
-#     print('satu')
-#     await asyncio.sleep(1)
-#     print('done')
-
-# async def dua():
-#     print('dua')
-#     await asyncio.sleep(1)
-#     print('done')
+        print("Getting Memory Usage: Done")
