6849387b16b8fc407b72dde96ea2388c1eebaa92
faizruzain <faizruzain@hotmail.com>
2025-12-31 20:13:05 +0700

    accepting user input

--- app.py
+++ app.py
@@ -1,37 +1,32 @@
 import asyncio
 import sys
 sys.path.append('my_modules')
-import requests
 import pandas as pd
-import json
-import csv
-import os
-from dotenv import load_dotenv
+
 from myModule import read_csv, get_CPU_usage, get_temperature_records, get_memory_usage
 
-load_dotenv()
+async def main():
+    print('Choose one of them!:')
+    print("""
+1 = CPU Data
+2 = Memory Data
+3 = Ping Data
+4 = Temperature Data
+5 = Traffic Data
+""")
+    user_input = int(input('Choose 1 to 5: '))
+    df = await read_csv()
+    await get_CPU_usage(df, user_input)
+    # await get_temperature_records(df)
+    # await get_memory_usage(df)
 
-# async def main():
-#     df = await read_csv()
-#     await get_CPU_usage(df)
-#     # await get_temperature_records(df)
-#     # await get_memory_usage(df)
+asyncio.run(main())
 
-# asyncio.run(main())
-my_token = os.getenv("API_KEY")
-id = '59358'
-url = f'https://10.164.1.101/api/historicdata.csv?id={id}&avg=86400&sdate=2025-12-01-00-00-00&edate=2025-12-30-23-59-59&apitoken={my_token}'
-# r = requests.get(url, verify=False)
-# print(r.status_code)
-# print(r.encoding)
-# avg_str = r.text.find('Averages')
-# print(r.text[avg_str::])
 
-# with open('output_csv/ping.csv', 'w') as f:
-#     f.write(r.text)
-
-# df = pd.read_csv('output_csv/traffic.csv')
+# df = pd.read_csv('cleaned_csv/temp.csv')
 # print(df)
+# df.to_csv('cleaned_csv/traffic.csv')
 # print(r.text)
 # with open('cuki.json', 'w') as f:
 #     json.dump(r, f)
+# encoding='unicode_escape'
--- my_modules/myModule.py
+++ my_modules/myModule.py
@@ -1,27 +1,52 @@
-import os
 import pandas as pd
 import requests
+from dotenv import load_dotenv
 from tabulate import tabulate
 
+load_dotenv()
+
+"""
+1 = CPU Data
+2 = Memory Data
+3 = Ping Data
+4 = Temperature Data
+5 = Traffic Data
+"""
 
 async def read_csv():
     try:
-        script_dir = os.path.dirname(__file__)
-        rel_path = "../csv/device_id.csv"
-        abs_file_path = os.path.join(script_dir, rel_path)
-        df = pd.read_csv(abs_file_path)
+        df = pd.read_csv('csv/device_id_dev.csv')
+        print(df)
         return df
     except NameError:
         print(NameError)
 
-async def get_CPU_usage(df):
+async def get_CPU_usage(df, user_input):
     try:
+        print(f'User chose: {user_input}')
+        # my_token = os.getenv("API_KEY")
+        # r = requests.get(url, verify=False)
+        # print(r.status_code)
+        # print(r.encoding)
+        # avg_str = r.text.find('Averages')
+        # print(r.text[avg_str::])
+
+        # with open('output_csv/ping.csv', 'w') as f:
+        #     f.write(r.text)
+        rows = df.get(["hostname"])
+        rows = rows.size
+        count = 1
         CPU_usage = df.get(["hostname", "cpu_id"])
+        # print(f'{count}/{rows}')
+
         for _ in CPU_usage.itertuples(index=False):
+            print(f'{count}/{rows}')
             # make API req
+            # url = f'https://10.164.1.101/api/historicdata.csv?id={_[1]}&avg=86400&sdate=2025-12-01-00-00-00&edate=2025-12-30-23-59-59&apitoken={my_token}'
+            # r = requests.get(url, verify=False)
             r = requests.get(f'https://jsonplaceholder.typicode.com/users/9')
             if r.status_code == 200:
-                print(r)
+                # print(r)
 
                 # print results
                 print(tabulate([
@@ -31,13 +56,28 @@ async def get_CPU_usage(df):
                     tablefmt="github"
                     )
                 )
-
+                count += 1
                 print('\n')
+            elif r.status_code != 200:
+                count += 1
+                print(f'The sensor ID: {_[1]} is not right.')
+                print('\n')
+                print(tabulate([
+                    [_[0], _[1], 'NOK']
+                ],
+                    headers=['hostname', 'cpu_id', 'status'],
+                    tablefmt="github"
+                    )
+                )
+                print('\n')
+                continue
             else:
                 print(f'Error occurred with status code: {r.status_code}')
                 break
     except NameError:
         print(NameError)
+    else:
+        print('All Done')
         
 
 async def get_temperature_records(df):
