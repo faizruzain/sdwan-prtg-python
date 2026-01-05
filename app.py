4b9ae7debdf2dd6a72dd444a7a1cba367fce3a5b
user <user@MSI-NB138XRY.mastersystem.co.id>
2026-01-05 14:14:15 +0700

    playing with DataFrame.loc[]

--- app.py
+++ app.py
@@ -8,29 +8,35 @@ from os import listdir
 from myModule import read_csv, get_things_done_fast
 from io import StringIO
 
-async def main():
-    print('Choose one of them!:')
-    print("""
-1 = CPU Data
-2 = Memory Data
-3 = Ping Data
-4 = Temperature Data
-5 = Traffic Data
-""")
-    user_input = int(input('Choose 1 to 5: '))
-    df = await read_csv()
-    await get_things_done_fast(df, user_input)
+# async def main():
+#     print('Choose one of them!:')
+#     print("""
+# 1 = CPU Data
+# 2 = Memory Data
+# 3 = Ping Data
+# 4 = Temperature Data
+# 5 = Traffic Data
+# """)
+#     user_input = int(input('Choose 1 to 5: '))
+#     df = await read_csv()
+#     await get_things_done_fast(df, user_input)
 
-asyncio.run(main())
+# asyncio.run(main())
+hostname_df = pd.read_csv('csv/device_id_new.csv', index_col=False)
+hostname_df = hostname_df.get(['hostname', 'cpu_id'])
+# rows = hostname_df.size
+# temporary = []
 
-# t = []
-# df = pd.read_csv('cleaned_csv/temp.csv')
-# size = df.get('Temperature Slot 6 - Temp: CP-CPU(RAW)').size
-# print(size, type(size))
-# columns = df.columns
-# for _ in range(size):
-#     t.append(_+100)
-# print(t)
-# df.insert(2, 'cuki', t)
+# for row in range(rows):
+#     temporary.append(0)
+
+# print(temporary)
+# hostname_df.insert(1, 'temporary', temporary)
+hostname_df.loc[[0],['temporary', 'qwerty']] = [12312, 'polo']
+# hostname_df.to_csv('output_csv/temporary.csv', index=False)
+print(hostname_df)
+# print('\n')
+# df = pd.read_csv('cleaned_csv/traffic.csv', index_col=False)
+# df = df.get(['Traffic Total (Volume)(RAW)', 'Traffic Total (Speed)(RAW)', 'Traffic In (Volume)(RAW)', 'Traffic Out (Volume)(RAW)'])
+# df = df.mean()
 # print(df)
-# print(len(columns))
