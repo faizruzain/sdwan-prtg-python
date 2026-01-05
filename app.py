96d0ad96de73e99d6c62979ed84c0cd8c8a832b1
faizruzain <faizruzain@hotmail.com>
2026-01-05 22:04:13 +0700

    still playing with .loc[]

--- app.py
+++ app.py
@@ -8,35 +8,35 @@ from os import listdir
 from myModule import read_csv, get_things_done_fast
 from io import StringIO
 
-# async def main():
-#     print('Choose one of them!:')
-#     print("""
-# 1 = CPU Data
-# 2 = Memory Data
-# 3 = Ping Data
-# 4 = Temperature Data
-# 5 = Traffic Data
-# """)
-#     user_input = int(input('Choose 1 to 5: '))
-#     df = await read_csv()
-#     await get_things_done_fast(df, user_input)
+async def main():
+    print('Choose one of them!:')
+    print("""
+1 = CPU Data
+2 = Memory Data
+3 = Ping Data
+4 = Temperature Data
+5 = Traffic Data
+""")
+    user_input = int(input('Choose 1 to 5: '))
+    df = await read_csv()
+    await get_things_done_fast(df, user_input)
 
-# asyncio.run(main())
-hostname_df = pd.read_csv('csv/device_id_new.csv', index_col=False)
-hostname_df = hostname_df.get(['hostname', 'cpu_id'])
-# rows = hostname_df.size
-# temporary = []
+asyncio.run(main())
 
-# for row in range(rows):
-#     temporary.append(0)
+# loc = [['Ping Time(RAW)', 'Minimum(RAW)', 'Maximum(RAW)', 'Packet Loss(RAW)']]
+# hostnames_df = pd.read_csv('csv/device_id_new.csv', index_col=False)
+# hostnames_df = hostnames_df.get(['hostname', 'cpu_id'])
+# print(hostnames_df)
+# for _ in hostnames_df.itertuples():
+#     print(_[0])
 
-# print(temporary)
-# hostname_df.insert(1, 'temporary', temporary)
-hostname_df.loc[[0],['temporary', 'qwerty']] = [12312, 'polo']
-# hostname_df.to_csv('output_csv/temporary.csv', index=False)
-print(hostname_df)
-# print('\n')
-# df = pd.read_csv('cleaned_csv/traffic.csv', index_col=False)
-# df = df.get(['Traffic Total (Volume)(RAW)', 'Traffic Total (Speed)(RAW)', 'Traffic In (Volume)(RAW)', 'Traffic Out (Volume)(RAW)'])
-# df = df.mean()
-# print(df)
+# data_df = pd.read_csv('cleaned_csv/ping.csv', index_col=False)
+# data_s = round(data_df.get(['Ping Time(RAW)', 'Minimum(RAW)', 'Maximum(RAW)', 'Packet Loss(RAW)']).mean())
+# print(data_s)
+# data=[]
+# for _ in data_s:
+#     data.append(_)
+# print(data)
+# hostnames_df.loc[[0], loc[0]] = data
+# print(data_s)
+# print(hostnames_df)
--- my_modules/myModule.py
+++ my_modules/myModule.py
@@ -32,87 +32,87 @@ async def get_things_done_fast(df, user_input):
     try:
         print(f'User chose: {user_input}')
         my_token = os.getenv("API_KEY")
-        # r = requests.get(url, verify=False)
         hostnames = df.get([0])
         print(hostnames)
         rows = hostnames.size
         count = 1
         task_lists = df.get([0, user_input])
-        # print(f'{count}/{rows}')
-        what_to_get = ''
+        what_to_get = []
         file_name = ''
         table = []
         headers = ['hostname', 'status']
+        loc = []
         if user_input == 1:
-            what_to_get = 'CPU 7(RAW)'
+            loc.insert(0, ['average_cpu'])
+            what_to_get.append(['CPU 7(RAW)'])
             file_name = f'cpu_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M')}'
             headers.insert(1, 'cpu_id')
             headers.insert(2, 'average_cpu')
         elif user_input == 2:
-            what_to_get = 'Percent Available Memory 1 (Processor)(RAW)'
+            loc.insert(0, ['average_memory'])
+            what_to_get.append(['Percent Available Memory 1 (Processor)(RAW)'])
             file_name = f'memory_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M')}'
             headers.insert(1, 'mem_id')
             headers.insert(2, 'average_memory')
         elif user_input == 3:
-            what_to_get = ['Ping Time(RAW)', 'Minimum(RAW)', 'Maximum(RAW)', 'Packet Loss(RAW)']
+            loc.insert(0, ['average_ping'])
+            what_to_get.append(['Ping Time(RAW)', 'Minimum(RAW)', 'Maximum(RAW)', 'Packet Loss(RAW)'])
             file_name = f'ping_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M')}'
             headers.insert(1, 'ping_id')
             headers.insert(2, 'average_ping')
         elif user_input == 4:
-            what_to_get = 'Temperature Slot 6 - Temp: CP-CPU(RAW)'
+            loc.insert(0, ['average_temperature', 'min', 'max'])
+            what_to_get.append(['Temperature Slot 6 - Temp: CP-CPU(RAW)'])
             file_name = f'temperature_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M')}'
             headers.insert(1, 'temp_id')
             headers.insert(2, 'average_temperature')
             headers.insert(3, 'min')
             headers.insert(4, 'max')
         elif user_input == 5:
-            what_to_get = ['Traffic Total (Volume)(RAW)', 'Traffic Total (Speed)(RAW)', 'Traffic In (Volume)(RAW)', 'Traffic Out (Volume)(RAW)']
+            loc.insert(0, ['average_traffic'])
+            what_to_get.append(['Traffic Total (Volume)(RAW)', 'Traffic Total (Speed)(RAW)', 'Traffic In (Volume)(RAW)', 'Traffic Out (Volume)(RAW)'])
             file_name = f'traffic_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M')}'
             headers.insert(1, 'traffic_id')
             headers.insert(2, 'average_traffic')
         gathered_val = []
-
-        for _ in task_lists.itertuples(index=False):
+        for _ in task_lists.itertuples():
             # make API req
-            url2 = f'https://afr-sdwan.free.beeceptor.com/api/prtg?id={_[1]}'
-            url1 = f'https://10.164.1.101/api/historicdata.csv?id={_[1]}&avg=86400&sdate=2025-12-01-00-00-00&edate=2025-12-30-23-59-59&apitoken={my_token}'
+            url2 = f'https://afr-sdwan.free.beeceptor.com/api/prtg?id={_[2]}'
+            url1 = f'https://10.164.1.101/api/historicdata.csv?id={_[2]}&avg=86400&sdate=2025-12-01-00-00-00&edate=2025-12-30-23-59-59&apitoken={my_token}'
             url3 = f'https://jsonplaceholder.typicode.com/users/9'
             # r = requests.get(url, verify=False)
             r = requests.get(url2, timeout=10)
-            r_f = StringIO(r.text)
-            r_df = pd.read_csv(r_f)
-            # what_to_get
-            r_df = r_df.get('cpu_usage')
-            r_df = round(r_df.mean())
-            gathered_val.append(r_df)
-
-            if r.status_code == 200:
-                # hostnames.loc[[0], [headers]] = [r_df]
-                table.append([_[0], _[1], r_df, 'Done'])
+            if r.status_code == 200 and r.headers['Content-Type'] == 'text/csv;charset=utf-8':
+                r_f = StringIO(r.text)
+                r_df = pd.read_csv(r_f)
+                r_df = r_df.get(what_to_get[0])
+                r_df = round(r_df.mean())
+                gathered_val.append(r_df)
+                data = []                
+                for d in r_df:
+                    data.append(d)
+                print(data)
+                hostnames.loc[_[0], loc[0]] = data
+                table.append([_[1], _[2], r_df, 'Done'])
                 print(tabulate(table, headers, tablefmt='simple_grid'))
                 print(f'{count} of {rows}')
                 count += 1
                 print('\n')
-            elif r.status_code != 200:
-                count += 1
-                print(f'Something is not right')
-                print('\n')
-                table.append([_[0], _[1], r_df, 'Skipped'])
+            elif r.status_code != 200 or r.headers['Content-Type'] != 'text/csv;charset=utf-8':
+                table.append([_[1], _[2], 'NaN', 'Skipped'])
                 print(tabulate(table, headers, tablefmt='simple_grid'))
+                print(f'{count} of {rows}')
+                count += 1
                 print('\n')
                 continue
             else:
-                print(f'Error occurred with status code: {r.status_code}')
+                print(f'Error occurred with status code: {r.status_code} and Content-Type: {r.headers['Content-Type']}')
                 break
-        # df.insert(2, headers[1], gathered_val)
-        print(f'{headers[2]} {gathered_val}')
-        # final data to save
-        # hostnames.insert(1, headers[2], gathered_val)
     except NameError:
         print(NameError)
     else:
         print('All Done')
     finally:
         # saving data to csv regardless success or error
-        hostnames.insert(1, headers[2], gathered_val)
-        hostnames.to_csv(f'output_csv/{file_name}.csv', index_label=None)
+        print(hostnames)
+        # hostnames.to_csv(f'output_csv/{file_name}.csv', index_label=None)
