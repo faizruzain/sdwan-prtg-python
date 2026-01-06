98612fbaa83756f316ed73471b66f89384cb4182
faizruzain <faizruzain@hotmail.com>
2026-01-06 20:59:25 +0700

    creating dummies

--- my_modules/dummy_csv.py
+++ my_modules/dummy_csv.py
@@ -1,22 +1,18 @@
 import pandas as pd
 import random
 
+get = ['Traffic Total (Volume)(RAW)', 'Traffic Total (Speed)(RAW)', 'Traffic In (Volume)(RAW)', 'Traffic Out (Volume)(RAW)']
+
 traffic_df = pd.read_csv('cleaned_csv/traffic.csv')
-traffic_df = traffic_df.get(['Date Time', 'Traffic Total (Volume)(RAW)', 'Traffic Total (Speed)(RAW)', 'Traffic In (Volume)(RAW)', 'Traffic Out (Volume)(RAW)'])
+traffic_df = traffic_df.get(get)
 traffic_df = traffic_df.drop(labels=[29, 30], axis=0)
-print(traffic_df)
-columns = traffic_df.columns
-traffic_df = traffic_df.loc[0, columns] = [1]
 
-# for _ in traffic_df.itertuples(index=True):
-#     # index = _[0]
-#     # location or columns = 1 to 5
-#     print(_)
-#     random_integer = []
+for col in traffic_df.columns:
 
-#     for i in range(29):
-#         random_integer.append(random.randint(1, 21))
-    
-#     traffic_df.loc[_[0], _[0]] = random_integer
+    for _ in traffic_df.itertuples(index=True):
+        # index = _[0]
+        # location or columns = 1 to 5
+        traffic_df.loc[_[0], col] = random.randint(1, 300)
 
 print(traffic_df)
+traffic_df.to_csv('output_csv/random3.csv', index=False)
