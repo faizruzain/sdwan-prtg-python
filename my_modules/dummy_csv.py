87bd18103f27181e22124f659df5d460e5913ebb
user <user@MSI-NB138XRY.mastersystem.co.id>
2026-01-08 14:48:35 +0700

    playing with dummy

--- my_modules/dummy_csv.py
+++ my_modules/dummy_csv.py
@@ -1,18 +1,38 @@
 import pandas as pd
 import random
 
-get = ['Traffic Total (Volume)(RAW)', 'Traffic Total (Speed)(RAW)', 'Traffic In (Volume)(RAW)', 'Traffic Out (Volume)(RAW)']
+string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+serial_number = ''
+ip_address = '172.16.16.' # /24
 
-traffic_df = pd.read_csv('cleaned_csv/traffic.csv')
-traffic_df = traffic_df.get(get)
-traffic_df = traffic_df.drop(labels=[29, 30], axis=0)
+for i in range(12):
+    serial_number += string[random.randint(0, len(string)-1)]
+# print(serial_number)
 
-for col in traffic_df.columns:
+status = [
+    'Active',
+    'Dead',
+]
 
-    for _ in traffic_df.itertuples(index=True):
-        # index = _[0]
-        # location or columns = 1 to 5
-        traffic_df.loc[_[0], col] = random.randint(1, 300)
+data = (
+    {
+        'device': '',
+        'ip_address': '',
+        'serial_number': '',
+        'status': ''
+    }
+)
 
-print(traffic_df)
-traffic_df.to_csv('output_csv/random3.csv', index=False)
+data_df = pd.DataFrame(data, index=[0])
+
+for column in data_df.columns:
+    for row in range(30):
+        for i in range(12):
+            serial_number += string[random.randint(0, len(string)-1)]
+        data_df.loc[row, 'device'] = row+1
+        data_df.loc[row, 'ip_address'] = f'{ip_address + str(row+1)}'
+        data_df.loc[row, 'serial_number'] = serial_number
+        data_df.loc[row, 'status'] = status[random.randint(0, 1)]
+        serial_number = ''
+
+print(data_df)
