bf26037215de00d462d296e93b1aface781fee17
faizruzain <faizruzain@hotmail.com>
2026-01-08 21:49:06 +0700

    creating dummy csv

--- my_modules/dummy_csv.py
+++ my_modules/dummy_csv.py
@@ -1,25 +1,19 @@
 import pandas as pd
 import random
 
-string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-serial_number = ''
-ip_address = '172.16.16.' # /24
-
-for i in range(12):
-    serial_number += string[random.randint(0, len(string)-1)]
-# print(serial_number)
-
-status = [
-    'Active',
-    'Dead',
-]
-
 data = (
     {
-        'device': '',
-        'ip_address': '',
-        'serial_number': '',
-        'status': ''
+        'CPU 7(RAW)': 0,
+        'Percent Available Memory 1 (Processor)(RAW)': 0,
+        'Ping Time(RAW)': 0,
+        'Minimum(RAW)': 0,
+        'Maximum(RAW)': 0,
+        'Packet Loss(RAW)': 0,
+        'Temperature Slot 6 - Temp: CP-CPU(RAW)': 0,
+        'Traffic Total (Volume)(RAW)': 0,
+        'Traffic Total (Speed)(RAW)': 0,
+        'Traffic In (Volume)(RAW)': 0,
+        'Traffic Out (Volume)(RAW)': 0
     }
 )
 
@@ -27,12 +21,82 @@ data_df = pd.DataFrame(data, index=[0])
 
 for column in data_df.columns:
     for row in range(30):
-        for i in range(12):
-            serial_number += string[random.randint(0, len(string)-1)]
-        data_df.loc[row, 'device'] = row+1
-        data_df.loc[row, 'ip_address'] = f'{ip_address + str(row+1)}'
-        data_df.loc[row, 'serial_number'] = serial_number
-        data_df.loc[row, 'status'] = status[random.randint(0, 1)]
-        serial_number = ''
+        if column == 'Ping Time(RAW)' or column == 'Minimum(RAW)' or column == 'Maximum(RAW)':
+            data_df.loc[row, column] = random.randint(3, 5)
+        elif column == 'Packet Loss(RAW)':
+            data_df.loc[row, column] = random.randint(1, 4)
+        elif column == 'Temperature Slot 6 - Temp: CP-CPU(RAW)':
+            data_df.loc[row, column] = random.randint(30, 39)
+        elif column == 'CPU 7(RAW)':
+            data_df.loc[row, column] = random.randint(1, 100)
+        else:
+            data_df.loc[row, column] = random.randint(1000, 10000)
 
 print(data_df)
+
+test = pd.read_csv('https://afr-sdwan.free.beeceptor.com/api/prtg?id=')
+print(test)
+# data_df.to_csv('output_csv/prtg_dummy.csv', index=False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+# serial_number = ''
+# ip_address = '172.16.16.' # /24
+
+# for i in range(12):
+#     serial_number += string[random.randint(0, len(string)-1)]
+# # print(serial_number)
+
+# status = [
+#     'Active',
+#     'Dead',
+# ]
+
+# data = (
+#     {
+#         'device': '',
+#         'ip_address': '',
+#         'serial_number': '',
+#         'status': ''
+#     }
+# )
+
+# data_df = pd.DataFrame(data, index=[0])
+
+# for column in data_df.columns:
+#     for row in range(30):
+#         for i in range(12):
+#             serial_number += string[random.randint(0, len(string)-1)]
+#         data_df.loc[row, 'device'] = row+1
+#         data_df.loc[row, 'ip_address'] = f'{ip_address + str(row+1)}'
+#         data_df.loc[row, 'serial_number'] = serial_number
+#         data_df.loc[row, 'status'] = status[random.randint(0, 1)]
+#         serial_number = ''
+
+# print(data_df)
