abbb60b5148e0965ee65615f0a6b5632a230f7a6
user <user@MSI-NB138XRY.mastersystem.co.id>
2026-01-09 13:33:27 +0700

    option #6

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
 import asyncio
 import sys
 sys.path.append('my_modules')
-import pandas as pd
-import requests
-import random
-from os import listdir
 from myModule import read_csv, get_things_done_fast
-from io import StringIO
 
 async def main():
     print('Choose one of them!:')
@@ -16,27 +11,14 @@ async def main():
 3 = Ping Data
 4 = Temperature Data
 5 = Traffic Data
+6 = DUDUK MANIS ONGKANG - ONGKANG KAKI
 """)
-    user_input = int(input('Choose 1 to 5: '))
+    user_input = int(input('Choose 1 to 6: '))
     df = await read_csv()
-    await get_things_done_fast(df, user_input)
+    if user_input == 6:
+        for i in range(5):
+            await get_things_done_fast(df, i+1)
+    else:
+        await get_things_done_fast(df, user_input)
 
 asyncio.run(main())
-
-# loc = [['Ping Time(RAW)', 'Minimum(RAW)', 'Maximum(RAW)', 'Packet Loss(RAW)']]
-# hostnames_df = pd.read_csv('csv/device_id_new.csv', index_col=False)
-# hostnames_df = hostnames_df.get(['hostname', 'cpu_id'])
-# print(hostnames_df)
-# for _ in hostnames_df.itertuples():
-#     print(_[0])
-
-# data_df = pd.read_csv('cleaned_csv/ping.csv', index_col=False)
-# data_s = round(data_df.get(['Ping Time(RAW)', 'Minimum(RAW)', 'Maximum(RAW)', 'Packet Loss(RAW)']).mean())
-# print(data_s)
-# data=[]
-# for _ in data_s:
-#     data.append(_)
-# print(data)
-# hostnames_df.loc[[0], loc[0]] = data
-# print(data_s)
-# print(hostnames_df)
