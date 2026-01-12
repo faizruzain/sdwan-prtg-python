990d14c5007e2b47598dbade2543d1ec958ecd0c
user <user@MSI-NB138XRY.mastersystem.co.id>
2026-01-12 15:31:26 +0700

    playing

--- my_modules/dummy_csv.py
+++ my_modules/dummy_csv.py
@@ -2,9 +2,9 @@ import pandas as pd
 import random
 
 
-df = pd.read_csv('cleaned_csv/temp.csv')
-df = df.get(['Temperature Slot 6 - Temp: CP-CPU(RAW)'])
-print(df.max().iloc[0])
+# df = pd.read_csv('cleaned_csv/temp.csv')
+# df = df.get(['Temperature Slot 6 - Temp: CP-CPU(RAW)'])
+# print(df.max().iloc[0])
 
 # ===============================================================
 
@@ -54,38 +54,40 @@ print(df.max().iloc[0])
 
 # ===============================================================
 
-# string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-# serial_number = ''
-# ip_address = '172.16.16.' # /24
+string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+serial_number = ''
+ip_address = '172.16.16.' # /24
 
-# for i in range(12):
-#     serial_number += string[random.randint(0, len(string)-1)]
-# # print(serial_number)
+for i in range(12):
+    serial_number += string[random.randint(0, len(string)-1)]
+# print(serial_number)
 
-# status = [
-#     'Active',
-#     'Dead',
-# ]
+status = [
+    'Active',
+    'Dead',
+]
 
-# data = (
-#     {
-#         'device': '',
-#         'ip_address': '',
-#         'serial_number': '',
-#         'status': ''
-#     }
-# )
+data = (
+    {
+        'device': '',
+        'ip_address': '',
+        'serial_number': '',
+        'status': ''
+    }
+)
 
-# data_df = pd.DataFrame(data, index=[0])
+data_df = pd.DataFrame(data, index=[0])
 
-# for column in data_df.columns:
-#     for row in range(30):
-#         for i in range(12):
-#             serial_number += string[random.randint(0, len(string)-1)]
-#         data_df.loc[row, 'device'] = row+1
-#         data_df.loc[row, 'ip_address'] = f'{ip_address + str(row+1)}'
-#         data_df.loc[row, 'serial_number'] = serial_number
-#         data_df.loc[row, 'status'] = status[random.randint(0, 1)]
-#         serial_number = ''
+for column in data_df.columns:
+    for row in range(30):
+        for i in range(9):
+            serial_number += string[random.randint(0, len(string)-1)]
+        data_df.loc[row, 'device'] = row+1
+        data_df.loc[row, 'ip_address'] = f'{ip_address + str(row+1)}'
+        data_df.loc[row, 'serial_number'] = f'XYZ{serial_number}'
+        data_df.loc[row, 'status'] = status[random.randint(0, 1)]
+        serial_number = ''
 
-# print(data_df)
+# filtering 'Dead'
+
+print(data_df.status)
